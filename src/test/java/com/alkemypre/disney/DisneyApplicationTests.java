cb3d40d7451833371b43cbc3b695becb07a030fd
Keddy2018 <recostawalter@gmail.com>
2022-01-13 17:52:22 -0300

    Carga de Proyecto inicial

---
+++ src/test/java/com/alkemypre/disney/DisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemypre.disney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
